bb28d7ff67683c6c9e0a993360e6d6ec31530fdc
coder-manaal <143649641+coder-manaal@users.noreply.github.com>
2023-08-31 22:08:36 +0530

    Add files via upload

---
+++ Assignment2-Task2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+# Python program to print positive Numbers in a range
+
+start = int(input("Enter the starting values:"))
+end = int(input("Enter the ending values:"))
+
+for num in range(start, end + 1):
+    
+    if num >= 0:
+        print(num, end=" ")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
